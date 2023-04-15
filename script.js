61c2f1f188c0b1c965397c4d6aaa42d1d09329b9
n1mah <nima.heidari.n@gmail.com>
2023-04-15 11:21:51 +0330

    Add convert Method for Purge Padding & Margin

--- script.js
+++ script.js
@@ -13,7 +13,97 @@ let getClassList=(str)=>{
     str = str.split(" ");
     return [...str];
 }
-let convertMargin=(marginListClass)=>{
+let convertMarginTO=(marginListClass)=>{
+    marginListClass.forEach((value, index) => {
+        switch (value[0]) {
+            case "m":
+                mb=value[1];mt=value[1];mr=value[1];ml=value[1];
+                break;
+            case "mx":
+               mr=value[1];ml=value[1];
+                break;
+            case "my":
+                mb=value[1];mt=value[1];
+                break;
+            case "mr":
+                mr=value[1];
+                break;
+            case "ml":
+                ml=value[1];
+                break;
+            case "mt":
+                mt=value[1];
+                break;
+            case "mb":
+                mb=value[1];
+                break;
+        }
+    });
+    return [mt,mb,mr,ml];
+}
+let convertTOMargin=(mt=0,mb=0,mr=0,ml=0)=>{
+    if (mt===mb && mr===ml && mt===mr){
+        return ["m",mt];
+    }else if (mt===mb && mr===ml){
+        return [["my",mt],["mx",mr]];
+    }else if(mt===mb || mr===ml){
+        if (mt===mb){
+            return [["my",mt],["mr",mr],["ml",ml]];
+        }else{
+            return [["mx",mr],["mt",mt],["mb",mb]];
+
+        }
+    }else {
+        return [["mr",mr],["ml",ml],["mt",mt],["mb",mb]];
+    }
+}
+let convertPaddingTO=(paddingListClass)=>{
+    let pb,pt,pr,pl;
+    paddingListClass.forEach((value, index) => {
+        switch (value[0]) {
+            case "p":
+                pb=value[1];pt=value[1];pr=value[1];pl=value[1];
+                break;
+            case "px":
+                pr=value[1];pl=value[1];
+                break;
+            case "py":
+                pb=value[1];pt=value[1];
+                break;
+            case "pr":
+                pr=value[1];
+                break;
+            case "pl":
+                pl=value[1];
+                break;
+            case "pt":
+                pt=value[1];
+                break;
+            case "pb":
+                pb=value[1];
+                break;
+        }
+    });
+    return [pt,pb,pr,pl];
+}
+let convertTOPadding=(pt=0,pb=0,pr=0,pl=0)=>{
+    if (pt===pb && pr===pl && pt===pr){
+        return ["p",pt];
+    }else if (pt===pb && pr===pl){
+        return [["py",pt],["px",pr]];
+    }else if(pt===pb || pr===pl){
+        if (pt===pb){
+            return [["py",pt],["pr",pr],["pl",pl]];
+        }else{
+            return [["px",pr],["pt",pt],["pb",pb]];
+
+        }
+    }else {
+        return [["pr",pr],["pl",pl],["pt",pt],["pb",pb]];
+    }
+}
+
+/*let convertMargin=(marginListClass)=>{
     let str;
     // marginListClass=[["mx","20"],["mr","15"],["m","10"]];//test
     marginListClass.reverse();
@@ -48,7 +138,7 @@ let convertMargin=(marginListClass)=>{
         marginListClass.splice(value,1);
     })
     return marginListClass;
-}
+}*/
 
 function purgeClassNames(...classNames){
     // TODO write your code here
